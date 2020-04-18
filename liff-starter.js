1234478655707f39258cb851e169c8b5971a489f
i18furuta <fn.nk.2002@gmail.com>
2020-04-18 18:43:12 +0900

    fetch

--- liff-starter.js
+++ liff-starter.js
@@ -1,40 +1,45 @@
 window.onload = function() {
     const liffId = "1654083267-9dX8zMJO";
-    alert("onload");
     liff
         .init({
             liffId: liffId
         })
         .then(() => {
-            // start to use LIFF's api
-            alert("success");
             login();
-            initializeApp();
         })
         .catch((err) => {
             alert("ERROR");
         });
 };
 
+document.getElementById("btn").onclick = function () {
+    var name = document.getElementsByName("name");
+    var namekana = document.getElementsByName("namekana");
+    var gender = document.getElementsByName("gender");
+    var profile = liff.getProfile();
+    var userId = profile.userId();
+    sendData(name, namekana, gender, userId);
+    alert("send");
+}
+
 function login() {
     if (!liff.isLoggedIn()) {
         liff.login();
     }
 }
-/**
- * Initialize the app by calling functions handling individual app components
- */
-function initializeApp() {
-    var name = "";
-    var userId = "";
-    liff.getProfile()
-        .then(profile => {
-            name = profile.displayName;
-            userId = profile.userId;
-            alert(profile.displayName + " userId : " + profile.userId);
-        })
-        .catch((err) => {
-            alert("ERROR");
-        });
-    alert(liff.isLoggedIn());
+
+function sendData(name, namekana, gender, userId) {
+    const url = "https://script.google.com/macros/s/AKfycbzpx_oEizB-u9d2AAg_OfAGf_TtaQ7OiRIbbSMjPmD3Kp0SqQA/exec";
+
+    var data = {
+        request: "ADD",
+        name: name,
+        namekana: namekana,
+        gender: gender,
+        userId: userId
+    };
+    await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data)
+    });
 }
